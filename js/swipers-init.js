f882b2384c3d26cce317cb4675876c0d3905cf2d
asanheyaa <asanheyaa@gmail.com>
2026-05-19 17:51:22 +0300

    search-result-page

--- js/swipers-init.js
+++ js/swipers-init.js
@@ -112,9 +112,9 @@ if (inventoryHomeSwiper) {
 		spaceBetween: 30,
 		speed: 800,
 		slidesPerView: 1,
-		
-		observer: true,      
-		observeParents: true,  
+
+		observer: true,
+		observeParents: true,
 		loop: true,
 		pagination: {
 			el: '.home-locations__pagination',
@@ -135,7 +135,7 @@ if (reviewsHomeSwiper) {
 	const swiper = new Swiper(reviewsHomeSwiper, {
 		spaceBetween: 30,
 		speed: 800,
-		slidesPerView:1,
+		slidesPerView: 1,
 		pagination: {
 			el: '.body-home-reviews__progress-line',
 			type: 'progressbar',
@@ -180,7 +180,7 @@ if (statsHomeSwiper) {
 				centeredSlides: true,
 				speed: 800,
 				autoHeight: true,
-				
+
 				breakpoints: {
 					460: {
 						spaceBetween: 20,
@@ -196,5 +196,78 @@ if (statsHomeSwiper) {
 	window.addEventListener('resize', () => {
 		clearTimeout(resizeTimer);
 		resizeTimer = setTimeout(toggleMobileSwiper, 120);
+	});
+}
+
+// SEARCH RESULT PAGE ======================================================================================================================================
+
+const productPhotosSwipers = document.querySelectorAll('.products-body-search__swiper');
+if (productPhotosSwipers) {
+
+	productPhotosSwipers.forEach(productPhotosSwiper => {
+		const swiper = new Swiper(productPhotosSwiper, {
+			spaceBetween: 15,
+			pagination: {
+				el: '.products-body-search__pagination',
+			},
+			on: {
+				init: function () {
+					updateCustomFraction(this);
+				},
+				slideChange: function () {
+					updateCustomFraction(this);
+				},
+			}
+		});
+
+		function updateCustomFraction(swiperInstance) {
+			const parent = swiperInstance.el
+			const currentTextContainer = parent.querySelector('.swiper-pagination-text');
+			if (!currentTextContainer) return;
+
+			const current = swiperInstance.realIndex + 1;
+			const total = swiperInstance.slides.length;
+
+			const currentFormatted = current < 10 ? '0' + current : current;
+			const totalFormatted = total < 10 ? '0' + total : total;
+
+			currentTextContainer.innerHTML = `
+    <span class="current-num">${current}</span>
+    <span class="sep">/</span>
+    <span class="total-num">${total}</span>
+  `;
+		}
+	});
+
+}
+
+// View products grid
+
+const changeViewButtons = document.querySelectorAll('[data-change-view]');
+
+if (changeViewButtons) {
+	changeViewButtons.forEach(changeViewButton => {
+		changeViewButton.addEventListener('click', (e)=>{
+			
+			changeViewButtons.forEach(changeViewButton => {
+				changeViewButton.classList.remove('_active');
+			});
+			changeViewButton.classList.add('_active')
+
+			const dataset = changeViewButton.dataset.changeView
+			const productsWrapper = document.querySelector('.body-search-results')
+
+			const isRows = dataset === "rows" ? true : false
+
+			if (isRows) {
+				productsWrapper.classList.remove('_grid')
+			} else {
+				productsWrapper.classList.add('_grid')
+			}
+		})
+
+		
+
+		
 	});
 }
